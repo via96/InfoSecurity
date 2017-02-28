4516bbb08c4948c5f9f24aaa0fee72ce2f94efd3
via96 <via96rus@gmail.com>
2017-02-28 22:46:54 +0500

    hren

--- task4c/converter.cpp
+++ task4c/converter.cpp
@@ -7,41 +7,39 @@ Converter::Converter()
 
 QBitArray Converter::StrToBit(QString str)
 {
-    QByteArray byteSeries = QByteArray::fromStdString(str.toStdString());
-    QBitArray result(byteSeries.length()*8);
-    for (int i = 0; i < byteSeries.length(); ++i)
-    {
-        char temp = 1;
-        for (int j = 0; j < 8; ++j)
-        {
-            result[(i+1)*(j+1) - 1] = byteSeries[i] & temp;
-            temp << 1;
-        }
-    }
-    return result;
+    QByteArray bytes = QByteArray::fromStdString(str.toStdString());
+    QBitArray bits;
+    bits.resize(bytes.size()*8);
+    for(int i=0; i<bytes.count(); ++i)
+        for(int b=0; b<8; ++b)
+            bits.setBit(i*8+b, bytes.at(i)&(1<<(7-b)));
+    return bits;
 }
 
-QString Converter::BitToStr(QBitArray bitSeries)
+QString Converter::BitToStr(QBitArray bits)
 {
-    QByteArray byteSeries;
-    char c = 0;
-    int blokSize = 0;
-    for (int i = 0; i < bitSeries.size(); ++i)
-    {
-        char temp = 0;
-        if (blokSize < 8)
-        {
-            temp |= bitSeries[i];
-            temp << 7 - blokSize;
-            c |= temp;
-            blokSize++;
-        }
-        else
-        {
-            byteSeries.append(c);
-            blokSize = 0;
-            c = 0;
-        }
-    }
-    return QString::fromStdString(byteSeries.toStdString());
+    QByteArray bytes;
+    bytes.resize(bits.size()/8);
+    for(int j=0; j<bits.count(); ++j)
+        bytes[j/8] = (bytes.at(j/8) | ((bits[j]?1:0)<<(j%8)));
+    return QString::fromStdString(bytes.toStdString());
+}
+
+QBitArray Converter::BytesToBits(QByteArray bytes)
+{
+    QBitArray bits;
+    bits.resize(bytes.size()*8);
+    for(int i=0; i<bytes.count(); ++i)
+        for(int b=0; b<8; ++b)
+            bits.setBit(i*8+b, bytes.at(i)&(1<<(7-b)));
+    return bits;
+}
+
+QByteArray Converter::BitsToBytes(QBitArray bits)
+{
+    QByteArray bytes;
+    bytes.resize(bits.size()/8);
+    for(int j=0; j<bits.count(); ++j)
+        bytes[j/8] = (bytes.at(j/8) | ((bits[j]?1:0)<<(j%8)));
+    return bytes;
 }
